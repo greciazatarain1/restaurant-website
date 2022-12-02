a060aea6ac144d7ab34f383868275dc21ae8d044
Grecia <greciazatarain@gmail.com>
2022-12-02 15:58:02 +0100

    second commit

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,9 @@ module.exports = defineConfig({
         path.resolve(__dirname, './src/assets/scss/main.scss'),
       ]
     }
-  }
+  },
+
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/project-restaurant/'
+  : '/'
 });
